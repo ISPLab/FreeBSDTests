62ebf9d3715879a7b6ddb2fa7866b6f128d70a93
andreyorlov <for_orlov@mail.ru>
2019-06-19 21:56:38 +0300

    enlighmenttest

---
+++ Enlightment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EnlighmentTest
+{
+    public class Example
+    {
+#if WIN32
+    [STAThreadAttribute()]
+#endif
+        public static void Main()
+        {
+            // Initialize EFL and all UI components
+            Efl.All.Init(Efl.Components.Ui);
+
+            // Create a window and initialize it
+            Efl.Ui.Win win = new Efl.Ui.Win(Efl.App.AppMain);
+            // Set the window's title
+            win.SetText("Hello World");
+            // Request that the window is automatically hidden when the "close"
+            // button is pressed
+            win.SetAutohide(true);
+            // Window size must be explicitly set, otherwise it will be invisible
+            // due to its lack of content.
+            win.SetSize(new Eina.Size2D(360, 240));
+
+            // Start the EFL main loop
+            Efl.Ui.Config.Run();
+
+            // Shutdown EFL
+            Efl.All.Shutdown();
+        }
+    }
+}
