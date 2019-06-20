694c56b560c96382277f580ba1e6c0096216c50b
andreyorlov <for_orlov@mail.ru>
2019-06-20 16:35:37 +0300

    gtk

---
+++ GTKTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Gtk;
+namespace GTKTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+
+            //Create the Window
+
+            Window myWin = new Window("My first GTK# Application! ");
+            myWin.Resize(200, 200);
+
+            //Create a label and put some text in it.
+
+            Label myLabel = new Label();
+            myLabel.Text = "Hello World!!!!";
+
+            //Add the label to the form
+            myWin.Add(myLabel);
+
+            //Show Everything
+
+            myWin.ShowAll();
+
+            Application.Run();
+
+        }
+    }
+}
