443809a8c2919e35d43f70985a0787a72cb42dbd
andreyorlov <for_orlov@mail.ru>
2019-06-24 11:31:37 +0300

    ???

--- PlatformsForms/MainPage.xaml.cs
+++ PlatformsForms/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 
 namespace PlatformsForms
@@ -16,6 +18,29 @@ namespace PlatformsForms
         public MainPage()
         {
             InitializeComponent();
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+            paint.Style = SKPaintStyle.Fill;
+            paint.Color = SKColors.Blue;
+            canvas.DrawCircle(args.Info.Width / 2, args.Info.Height / 2, 100, paint);
         }
     }
 }
